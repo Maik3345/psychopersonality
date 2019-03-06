de067302c4bfc92c66491a6fb466626ac7b94e86
Maik Restrepo <stiven3345@hotmail.com>
2019-03-06 07:08:09 -0500

    Add docs folder

---
+++ docs/precache-manifest.a95da82d71b921b7bc8b1219292fe3f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "55f726c6655247324dda",
+    "url": "/static/js/main.55f726c6.chunk.js"
+  },
+  {
+    "revision": "b6d92abf27578961a18e",
+    "url": "/static/js/2.b6d92abf.chunk.js"
+  },
+  {
+    "revision": "55f726c6655247324dda",
+    "url": "/static/css/main.60a24c2c.chunk.css"
+  },
+  {
+    "revision": "36235783c24587736bcd2c134414976b",
+    "url": "/index.html"
+  }
+];
